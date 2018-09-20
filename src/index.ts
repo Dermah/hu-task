77f8658ddaa64b27662727b47a4b6e55112f3897
Sputnik <inquiry@dermah.com>
2018-09-20 22:25:28 +1000

    Check bookings start and end at the right times past the hour

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,27 @@ export const validateAndCostBooking = (booking: Booking): CostedBooking => {
   const to = moment(booking.to, TIMESTAMP_FORMAT);
 
   const duration = moment.duration(to.diff(from));
-  // The minimum booking time is 1 hour
-  // The maximum booking time is 24 hours
-  // A booking cannot end before it has started
-  if (duration.asHours() < 1 || duration.asHours() > 24) {
+  const allowedMinutes = { 0: true, 15: true, 30: true, 45: true };
+  if (
+    // The minimum booking time is 1 hour
+    // The maximum booking time is 24 hours
+    // A booking cannot end before it has started
+    duration.asHours() < 1 ||
+    duration.asHours() > 24
+  ) {
+    isValid = false;
+  } else if (
+    // A booking can be booked in 15 min increments e.g. 1600 to 1715
+    //   Note: unclear whether this means
+    //     * the duration must be a multiple of 15 minutes,
+    //     * that bookings must only begin at 0, 15, 30 or 45 minutes past the hour (logically includes the above condition)
+    //   Assuming the latter based on input.json
+    !allowedMinutes[from.minutes()] ||
+    !allowedMinutes[to.minutes()]
+  ) {
     isValid = false;
   }
 
-  // A booking can be booked in 15 min increments e.g. 1600 to 1715
   // If any part of a booking is charged at the night rate, the whole booking is charged at the night rate:
   // Fri 1800 - 2100 will be charged at the night rate (3 x 42.93)
   // Wed 0500 - 1000 will be charged at the night rate (5 x 42.93)

cbd903f07924bc6733059f429ebea76773ffff3e
Sputnik <inquiry@dermah.com>
2018-09-20 23:27:01 +1000

    Extract validator to its own file

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 import inputFixture from "./fixtures/input.json";
 import expectedFixture from "./fixtures/expectedOutput.json";
-import { validateAndCostBooking } from ".";
+import { validateAndCostBooking } from "./validateBookings";
 
 describe("Booking Validation Function", () => {
   inputFixture.map((input, i) => {

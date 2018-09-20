7dc048f458c6a2991d0729ce835363ad03ed5463
Sputnik <inquiry@dermah.com>
2018-09-20 22:35:32 +1000

    Add rates and weekend cost logic

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,13 @@ interface CostedBooking extends Booking {
 }
 
 const TIMESTAMP_FORMAT = "YYYY-MM-DDTHH:mm:ssZ";
+const ALLOWED_MINUTES = { 0: true, 15: true, 30: true, 45: true };
+const RATES = {
+  day: 38,
+  night: 42.93,
+  sat: 45.91,
+  sun: 60.85
+};
 
 export const validateAndCostBooking = (booking: Booking): CostedBooking => {
   let isValid = true;
@@ -21,7 +28,7 @@ export const validateAndCostBooking = (booking: Booking): CostedBooking => {
   const to = moment(booking.to, TIMESTAMP_FORMAT);
 
   const duration = moment.duration(to.diff(from));
-  const allowedMinutes = { 0: true, 15: true, 30: true, 45: true };
+
   if (
     // The minimum booking time is 1 hour
     // The maximum booking time is 24 hours
@@ -36,18 +43,32 @@ export const validateAndCostBooking = (booking: Booking): CostedBooking => {
     //     * the duration must be a multiple of 15 minutes,
     //     * that bookings must only begin at 0, 15, 30 or 45 minutes past the hour (logically includes the above condition)
     //   Assuming the latter based on input.json
-    !allowedMinutes[from.minutes()] ||
-    !allowedMinutes[to.minutes()]
+    !ALLOWED_MINUTES[from.minutes()] ||
+    !ALLOWED_MINUTES[to.minutes()]
   ) {
     isValid = false;
   }
 
-  // If any part of a booking is charged at the night rate, the whole booking is charged at the night rate:
-  // Fri 1800 - 2100 will be charged at the night rate (3 x 42.93)
-  // Wed 0500 - 1000 will be charged at the night rate (5 x 42.93)
-  // Saturday and Sunday rates apply across the whole day, there's no distinction between day and night:
-  // Sat 1800 - 2200 will be charged at the sat rate (4 x 45.91)
-  // Sun 0100 - 0700 will be charged at the sun rate (6 x 60.85)
+  if (isValid) {
+    // Saturday and Sunday rates apply across the whole day, there's no distinction between day and night:
+    // Sat 1800 - 2200 will be charged at the sat rate (4 x 45.91)
+    // Sun 0100 - 0700 will be charged at the sun rate (6 x 60.85)
+    if (
+      // Booking starts on a Sunday
+      from.day() === 0
+    ) {
+      total = RATES.sun * duration.asHours();
+    } else if (
+      // Booking starts on a Saturday
+      from.day() === 6
+    ) {
+      total = RATES.sat * duration.asHours();
+    } else {
+      // If any part of a booking is charged at the night rate, the whole booking is charged at the night rate:
+      // Fri 1800 - 2100 will be charged at the night rate (3 x 42.93)
+      // Wed 0500 - 1000 will be charged at the night rate (5 x 42.93)
+    }
+  }
 
   return {
     ...booking,

d3cf1fe806d705e86e717cc2c5e939245c20c5d2
Sputnik <inquiry@dermah.com>
2018-09-20 21:45:34 +1000

    Fix original booking fields and purposely fail total test

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,8 @@ interface CostedBooking extends Booking {
 
 export const validateAndCostBooking = (booking: Booking): CostedBooking => {
   return {
-    id: 2,
-    from: "2017-10-20T09:00:00+11:00",
-    to: "2017-10-20T11:45:00+11:00",
+    ...booking,
     isValid: true,
-    total: 104.5
+    total: 3000
   };
 };
